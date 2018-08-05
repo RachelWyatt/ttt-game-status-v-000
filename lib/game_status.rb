77f41259c6a8d28762124d99e834fea7ae79b211
Rachel Wyatt <rw126@evansville.edu>
2018-08-05 15:54:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,9 +26,9 @@ def won?(board)
   position_3 = board[win_index_3] # load the value of the board at win_index_3
 
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
-     return WIN_COMBINATIONS # return the win_combination indexes that won.
+     return combination # return the win_combination indexes that won.
    elseif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return WIN_COMBINATIONS
+      return combination
    end
    return false
 end
